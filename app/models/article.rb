c306462ea893cf816531d512ebe524d92465aee9
Alexander <mr.beerdy@gmail.com>
2018-03-05 14:39:59 +0300

    all

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,21 +5,8 @@ class Article < ActiveRecord::Base
     #exclude_fields  :sort, :updated_at, :created_at, :slave, :review, :url, :portfolio
 
     edit do
-      field :description, :rich_editor do
-        config({
-          :insert_many => true
-        })
-      end
-      field :slave, :rich_editor do
-        config({
-          :insert_many => true
-        })
-      end
-    end
-
-    edit do
       # For RailsAdmin >= 0.5.0
-      field :slave, :ck_editor
+      field :description, :ck_editor
       # For RailsAdmin < 0.5.0
       # field :description do
       #   ckeditor true
