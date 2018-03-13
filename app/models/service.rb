bab9bb90617a424b52e347767be1110138b26533
Alexander <mr.beerdy@gmail.com>
2018-03-13 14:18:06 +0300

    add service pages

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,17 @@
 class Service < ActiveRecord::Base
   dragonfly_accessor :image
+  rails_admin do
+    include_all_fields
+    
+    #exclude_fields  :sort, :updated_at, :created_at, :slave, :review, :url, :portfolio
+
+    edit do
+      # For RailsAdmin >= 0.5.0
+      field :description, :ck_editor
+      # For RailsAdmin < 0.5.0
+      # field :description do
+      #   ckeditor true
+      # end
+    end
+  end
 end
